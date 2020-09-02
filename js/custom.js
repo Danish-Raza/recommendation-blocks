565389662e0fa64256f1e3e76894ababc59cbffe
danish <danish.raza@dataweave.com>
2020-09-02 09:12:26 +0530

    added 2 blocks

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,265 @@
+var data = {
+  "Other Role-Playing Games": [
+    {
+      "discount": "",
+      "handle": "my-time-at-portia-xb1",
+      "clip":"assets/CYBERPUNK 2077 - Rmp4.mp4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/My_Time_at_Portia_Xb1_Cov.jpg?v=1592981794",
+      "price": "$5.00",
+      "product_id": 45459,
+      "short_text":"lorem ipsum",
+      "long_text":"Shoot enemies and gather amazing loot to upgrade and customize your character as you see fit.",
+      "title": "My Time at Portia"
+    },
+    {
+      "discount": "",
+      "handle": "ni-no-kuni-2-digital-princes-edition-win",
+      "clip":"assets/God of War - Rmp4.mp4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ni_no_Kuni_II_win_prince_cov.png?v=1592984299",
+      "product_id": 28974,
+      "price": "$10.00",
+      "title": "Ni no Kuni II: Revenant Kingdom - Digital Prince's Edition"
+    },
+    {
+      "discount": "",
+      "handle": "atelier-lulua-the-scion-of-arland-digital-deluxe-edition-win",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Atelier_Lulua_The_Scion_of_Arland_ps4_win_ns_deluxe_cov_601d6f28-39b5-4f7d-8738-c085b3560765.jpg?v=1592984407",
+      "product_id": 68115,
+      "price": "$15.00",
+      "clip":"assets/Mirror's Edge Catalyst RMp4.mp4",
+      "title": "Atelier Lulua: The Scion of Arland - Digital Deluxe Edition"
+    },
+    {
+      "discount": "",
+      "handle": "bless-online-win",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/BLESS_ONLINE_win_cov.jpg?v=1592984940",
+      "price": "$5.00",
+      "clip":"assets/Star Citizen - RMp4.mp4",
+      "product_id": 20050,
+      "title": "BLESS ONLINE"
+    },
+    {
+      "discount": "",
+      "handle": "atelier-lulua-the-scion-of-arland-digital-deluxe-edition-win",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Atelier_Lulua_The_Scion_of_Arland_ps4_win_ns_deluxe_cov_601d6f28-39b5-4f7d-8738-c085b3560765.jpg?v=1592984407",
+      "price": "$20.00",
+      "product_id": 68115,
+      "clip":"assets/Star Citizen - RMp4.mp4",
+      "title": "Atelier Lulua: The Scion of Arland - Digital Deluxe Edition"
+    },
+    {
+      "discount": "",
+      "handle": "bless-online-win",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/BLESS_ONLINE_win_cov.jpg?v=1592984940",
+      "price": "$5.00",
+      "product_id": 20050,
+      "clip":"assets/Star Citizen - RMp4.mp4",
+      "title": "BLESS ONLINE"
+    }
+  ],
+  "Other users like you also bought": [
+    {
+      "discount": "",
+      "handle": "agents-of-mayhem-xb1",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/game_cover_688d87af2ef876214d649b588469b7af.jpg?v=1592985340",
+      "price": "$5.00",
+      "product_id": "VGSXB1-139",
+      "short_text":"lorem ipsum",
+      "long_text":"Shoot enemies and gather amazing loot to upgrade and customize your character as you see fit.",
+      "title": "Agents of Mayhem"
+    },
+    {
+      "discount": "",
+      "handle": "days-gone-digital-deluxe-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days-Gone-Digital-Deluxe-Edition-ps4cvr.jpg?v=1592981531",
+      "price": "",
+      "product_id": "VGSPS4-3.1",
+      "short_text":"lorem ipsum",
+      "long_text":"Shoot enemies and gather amazing loot to upgrade and customize your character as you see fit.",
+      "title": "Days Gone: Digital Deluxe Edition"
+    },
+    {
+      "discount": "10%",
+      "handle": "ace-combat-7-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ace_Combat_7_PS4_Cov.png?v=1592981616",
+      "price": "$10.00",
+      "product_id": "VGSPS4-6",
+      "title": "Ace Combat 7: Skies Unknown"
+    },
+    {
+      "discount": "5%",
+      "handle": "days-gone-collectors-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days_Gone_CE_PS4_Cov.jpg?v=1592981533",
+      "price": "$5.00",
+      "product_id": "VGSPS4-3.2",
+      "title": "Days Gone: Collector’s Edition"
+    },
+    {
+      "discount": "",
+      "handle": "agents-of-mayhem-xb1",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/game_cover_688d87af2ef876214d649b588469b7af.jpg?v=1592985340",
+      "price": "$5.00",
+      "product_id": "VGSXB1-139",
+      "title": "Agents of Mayhem"
+    },
+    {
+      "discount": "",
+      "handle": "days-gone-digital-deluxe-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days-Gone-Digital-Deluxe-Edition-ps4cvr.jpg?v=1592981531",
+      "price": "",
+      "product_id": "VGSPS4-3.1",
+      "title": "Days Gone: Digital Deluxe Edition"
+    },
+    {
+      "discount": "10%",
+      "handle": "ace-combat-7-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ace_Combat_7_PS4_Cov.png?v=1592981616",
+      "price": "$10.00",
+      "product_id": "VGSPS4-6",
+      "title": "Ace Combat 7: Skies Unknown"
+    },
+    {
+      "discount": "5%",
+      "handle": "days-gone-collectors-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days_Gone_CE_PS4_Cov.jpg?v=1592981533",
+      "price": "$5.00",
+      "product_id": "VGSPS4-3.2",
+      "title": "Days Gone: Collector’s Edition"
+    },
+    {
+      "discount": "",
+      "handle": "agents-of-mayhem-xb1",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/game_cover_688d87af2ef876214d649b588469b7af.jpg?v=1592985340",
+      "price": "$5.00",
+      "product_id": "VGSXB1-139",
+      "title": "Agents of Mayhem"
+    },
+    {
+      "discount": "",
+      "handle": "days-gone-digital-deluxe-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days-Gone-Digital-Deluxe-Edition-ps4cvr.jpg?v=1592981531",
+      "price": "",
+      "product_id": "VGSPS4-3.1",
+      "title": "Days Gone: Digital Deluxe Edition"
+    },
+    {
+      "discount": "10%",
+      "handle": "ace-combat-7-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ace_Combat_7_PS4_Cov.png?v=1592981616",
+      "price": "$10.00",
+      "product_id": "VGSPS4-6",
+      "title": "Ace Combat 7: Skies Unknown"
+    },
+    {
+      "discount": "5%",
+      "handle": "days-gone-collectors-edition-ps4",
+      "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days_Gone_CE_PS4_Cov.jpg?v=1592981533",
+      "price": "$5.00",
+      "product_id": "VGSPS4-3.2",
+      "title": "Days Gone: Collector’s Edition"
+    }
+  ]
+
+}
 $(function () {
-    var cardHeight = 350
-    var owl = $('.owl-carousel');
-    $("#card-container").owlCarousel({
-        items: 4,
-        //nav:true,
-		responsive: {
-		  0: {
-			items: 1
-		  },
-		  480: {
-			items: 2
-		  },
-		  768: {
-			items: 3
-          },
-          1200: {
-            items: 4
-          }
-		}
-    });
-    owl.owlCarousel();
-    $('.customNextBtn').click(function() {
-        owl.trigger('next.owl.carousel');
-    })
-    $('.customPrevBtn').click(function() {
-        owl.trigger('prev.owl.carousel', [300,4]);
-    })
-    $('.customNextBtn').css({position:"relative",top:`${cardHeight/2}px`, left: "30px"});
-    $('.customPrevBtn').css({position:"relative",top:`${cardHeight/2}px`, right: "30px"});
+  var gameopediaRecommendationBlock = document.getElementById("gameopedia-recommendation-block");
+  var index = 0;
+  for (var recBlock in data) {
+    index++
+    let block = recomendationBlock(data[recBlock], index, recBlock);
+    gameopediaRecommendationBlock.innerHTML += block;
+  }
 });
+const playVideo = (video) => {
+  video.play(); 
+}
+const pauseVideo = (video) => {
+  video.load();
+}
+
+const recomendationBlock = (recData, index, recBlock) => {
+  let cards = ""
+  if (index == 1) {
+    recData.forEach((record,index) => {
+      cards+= `<div class="card">
+      <video poster="${record.img_src}" id="bgvid" playsinline muted loop onmouseover="playVideo(this)" onmouseleave="pauseVideo(this)">
+        <source src="${record.clip}">
+      </video>      
+      <div class="card-body">
+        <div class="card-header">
+          <div class="title">${record.title ? record.title : ""}</div>
+          <div class="short-text">${record.short_text ? record.short_text : ""}</div>
+          <div class="long-text">${record.long_text ? record.long_text : ""}</div>
+        </div>
+        <div class="card-footer">
+        ${record.price ? `<div class="price">${record.price}</div>`: ""}
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+      </div>
+      </div>
+    </div>`
+    });
+  } else if (index == 2) {
+    recData.forEach((record, index) => {
+      cards += `<div class="card-wrapper">
+      <div class="card-body">
+        <div class="card-footer">
+          <div class="long-text" style="color:white;text-align:center;padding:5px">${record.long_text ? record.long_text : ""}</div>
+          <div style="display:flex;justify-content: center; width: 120px">
+          <div class="price" style="color:#F8E71C;margin-right:5px">${record.price ? record.price :""}</div>
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="box-art" style="background-image: url(${record.img_src});"></div>
+      </div>             
+      <div class="title">${record.title}</div>
+      <div class="short-text">${record.short_text ? record.short_text : ""}</div>
+      </div>`
+    });
+  } else if(index == 3) {
+    recData.forEach((record, index) => {
+      let screenshots = "";
+      record.screenshots.forEach(element => {
+        screenshots+= ` <div class="box-art" style="background-image: url(${element});"></div>`
+      }); 
+      cards += `<div class="card-wrapper">
+      <div class="card-body">
+        <div class="card-header">
+          <div class="title">${record.title ? record.title : ""}</div>
+          <div class="short-text">${record.short_text ? record.short_text : ""}</div>
+          <div class="long-text">${record.long_text ? record.long_text : ""}</div>
+        </div>
+        <div class="card-footer">
+          <div class="add-to-cart"><i class="fa fa-shopping-cart" aria-hidden="true" style="margin-right:5px"></i> ADD TO CART</div> 
+          ${record.price ? `<div class="price">${record.price}</div>`: ""}
+        </div>
+      </div>
+      <div class="card">
+        <video poster="${record.img_src}" id="bgvid" playsinline muted loop onmouseover="playVideo(this)" onmouseleave="pauseVideo(this)">
+          <source src="${"assets/CYBERPUNK 2077 - Rmp4.mp4"}">
+         </video>
+          ${screenshots!="" ? `<div class="screenshots-wrapper">
+          <i class="fa fa-chevron-up" aria-hidden="true"></i>
+          <div class="screenshots">
+          ${screenshots}
+          </div>
+          <i class="fa fa-chevron-down" aria-hidden="true"></i>
+         </div> `: ""}
+      </div>
+      </div>`
+    });
+  }
+  return (
+    `<div class="content-box template-${index}">
+      <div class="content-title"> ${recBlock} </div>
+      <div class="container-wrapper">
+        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+        <div class="container" data-container-index="${index}">
+          ${cards}
+        </div>
+        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+      </div>
+    </div>`
+  )
+}
---
+++ js/data.js
@@ -0,0 +1,72 @@
+var data ={
+    "Other Role-Playing Games": [
+      {
+        "discount": "",
+        "handle": "my-time-at-portia-xb1",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/My_Time_at_Portia_Xb1_Cov.jpg?v=1592981794",
+        "price": "$5.00",
+        "product_id": 45459,
+        "title": "My Time at Portia (Xbox One)"
+      },
+      {
+        "discount": "",
+        "handle": "ni-no-kuni-2-digital-princes-edition-win",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ni_no_Kuni_II_win_prince_cov.png?v=1592984299",
+        "price": "",
+        "product_id": 28974,
+        "title": "Ni no Kuni II: Revenant Kingdom - Digital Prince's Edition (Windows)"
+      },
+      {
+        "discount": "",
+        "handle": "atelier-lulua-the-scion-of-arland-digital-deluxe-edition-win",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Atelier_Lulua_The_Scion_of_Arland_ps4_win_ns_deluxe_cov_601d6f28-39b5-4f7d-8738-c085b3560765.jpg?v=1592984407",
+        "price": "",
+        "product_id": 68115,
+        "title": "Atelier Lulua: The Scion of Arland - Digital Deluxe Edition (Windows)"
+      },
+      {
+        "discount": "",
+        "handle": "bless-online-win",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/BLESS_ONLINE_win_cov.jpg?v=1592984940",
+        "price": "$5.00",
+        "product_id": 20050,
+        "title": "BLESS ONLINE (Windows)"
+      }
+    ],
+    "Other users like you also bought": [
+      {
+        "discount": "",
+        "handle": "agents-of-mayhem-xb1",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/game_cover_688d87af2ef876214d649b588469b7af.jpg?v=1592985340",
+        "price": "$5.00",
+        "product_id": "VGSXB1-139",
+        "title": "Agents of Mayhem (Xbox One)"
+      },
+      {
+        "discount": "",
+        "handle": "days-gone-digital-deluxe-edition-ps4",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days-Gone-Digital-Deluxe-Edition-ps4cvr.jpg?v=1592981531",
+        "price": "",
+        "product_id": "VGSPS4-3.1",
+        "title": "Days Gone: Digital Deluxe Edition (PlayStation 4)"
+      },
+      {
+        "discount": "10%",
+        "handle": "ace-combat-7-ps4",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Ace_Combat_7_PS4_Cov.png?v=1592981616",
+        "price": "$10.00",
+        "product_id": "VGSPS4-6",
+        "title": "Ace Combat 7: Skies Unknown (PlayStation 4)"
+      },
+      {
+        "discount": "5%",
+        "handle": "days-gone-collectors-edition-ps4",
+        "img_src": "https://cdn.shopify.com/s/files/1/0280/6457/9655/products/Days_Gone_CE_PS4_Cov.jpg?v=1592981533",
+        "price": "$5.00",
+        "product_id": "VGSPS4-3.2",
+        "title": "Days Gone: Collector’s Edition (PlayStation 4)"
+      }
+    ]
+ 
+}
+export default data;
